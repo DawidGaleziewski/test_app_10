6b376e2449b4a3702263349538502e8fb153f419
DawidGaleziewski <dawidgalsino@vp.pl>
2018-09-05 18:43:46 +0000

    test change validation on app

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 2 , maximum: 10}, uniqueness: true
-  validates :description, presence: true, length: {minimum: 2 , maximum: 10}
+  validates :title, presence: true, length: {minimum: 2 , maximum: 30}, uniqueness: true
+  validates :description, presence: true, length: {minimum: 2 , maximum: 1000}
   validates :user_id, presence: true
 end
