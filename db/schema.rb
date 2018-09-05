51cfd299530646574c3d3b89eec330199b10b6eb
DawidGaleziewski <dawidgalsino@vp.pl>
2018-09-05 19:48:51 +0000

    updated datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_192926) do
+ActiveRecord::Schema.define(version: 2018_09_05_194034) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "timestamps"
     t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
